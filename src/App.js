461c26028c629fa3d487a4e7d58f9c92fcdd85ea
tina otarashvili <tinatinotarashvili99@gmail.com>
2024-03-10 00:55:03 +0400

    create footer section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 };
