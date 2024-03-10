cafdf2f4cf7751a8f1d5eb198939ea48e2321270
tina otarashvili <tinatinotarashvili99@gmail.com>
2024-03-10 17:58:18 +0400

    add landing content

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -6,6 +7,7 @@ const App = () => {
   return (
     <div className="App">
       <Header />
+      <Content />
       <Footer />
     </div>
   );
