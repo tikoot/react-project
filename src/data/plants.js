d5fa69e172674e01334434691576f6e4d3bd0489
tina otarashvili <tinatinotarashvili99@gmail.com>
2024-03-17 23:03:34 +0400

    create dummy data for products card

---
+++ src/data/plants.js
@@ -0,0 +1,45 @@
+import ZZ from "../assets/img/zz.webp";
+import Spider from "../assets/img/spider.webp";
+import Pothos from "../assets/img/pothos.webp";
+import Monstera from "../assets/img/monstera.webp";
+import Snake from "../assets/img/snake.webp";
+
+const plants = [
+  {
+    id: 1,
+    name: "Snake Plant",
+    description:
+      "The Snake Plant, or Mother-in-Law's Tongue, is a popular indoor plant known for its air-purifying qualities and easy care.",
+    image: Snake,
+  },
+  {
+    id: 2,
+    name: "Pothos",
+    description:
+      "Pothos, also known as Devil's Ivy, is a versatile and easy-to-care-for trailing plant with heart-shaped leaves.",
+    image: Pothos,
+  },
+  {
+    id: 3,
+    name: "Spider Plant",
+    description:
+      "Spider Plants are attractive and easy-to-grow houseplants, known for their arching leaves and air-purifying properties.",
+    image: Spider,
+  },
+  {
+    id: 4,
+    name: "ZZ Plant",
+    description:
+      "ZZ Plant, or Zamioculcas Zamiifolia, is a popular low-maintenance houseplant with glossy, dark green leaves.",
+    image: ZZ,
+  },
+  {
+    id: 5,
+    name: "Monstera",
+    description:
+      "Monstera, also known as the Swiss Cheese Plant, is a tropical plant with iconic split leaves, often grown as a houseplant.",
+    image: Monstera,
+  },
+];
+
+export default plants;
