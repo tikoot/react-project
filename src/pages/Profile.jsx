70fc8e1ba964e1f9428ac6c16d795427f9c0aed6
tina otarashvili <tinatinotarashvili99@gmail.com>
2024-03-26 23:59:35 +0400

    add profile static info and password inputs

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,84 @@
+import avatar from "../assets/img/avatar.png";
+
 const Profile = () => {
-  return <section className="flex flex-col h-screen"></section>;
+  return (
+    <section className="flex flex-col h-screen overflow-y-auto">
+      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            src={avatar}
+            alt="avatar"
+            class="h-[120px] w-auto rounded-full"
+          />
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form class="space-y-6" action="#">
+            <div class="mt-2">
+              <input
+                id="name"
+                name="name"
+                value="tiko"
+                readonly
+                className="disabled bg-transparent focus:outline-none border-b border-transparent capitalize"
+                style={{ "caret-color": "transparent" }}
+              />
+            </div>
+
+            <div class="mt-2">
+              <input
+                id="lastname"
+                name="lastname"
+                value="otarashvili"
+                readonly
+                className="disabled bg-transparent focus:outline-none border-b border-transparent capitalize"
+                style={{ "caret-color": "transparent" }}
+              />
+            </div>
+
+            <div class="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                value="tinatinotarashvili99@gmail.com"
+                className="disabled bg-transparent focus:outline-none border-b border-transparent w-full"
+                style={{ "caret-color": "transparent" }}
+              />
+            </div>
+            <div class="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="New Password"
+                required
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 pl-2 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div class="mt-2">
+              <input
+                id="confirmPassword"
+                name="confirmPassword"
+                type="password"
+                placeholder="Confirm Password"
+                required
+                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 pl-2 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                class="flex w-full justify-center rounded-md bg-[#B08EAD] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
