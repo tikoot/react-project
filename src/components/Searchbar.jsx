4bc9756e4d5e881ed18d8ac6e486cc7169d98306
tina otarashvili <tinatinotarashvili99@gmail.com>
2024-03-31 00:26:59 +0400

    add sort functionality

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,4 +1,4 @@
-const Searchbar = () => {
+const Searchbar = ({ onSortAsc, onSortDesc }) => {
   return (
     <div className="py-4">
       <div className="max-w-[790px] m-auto bg-[#f4edf2] py-[10px] pl-[32px] pr-[30px] text-lg text-[#666] rounded-[34px] flex items-center h-[60px]">
@@ -7,17 +7,18 @@ const Searchbar = () => {
           type="text"
           placeholder="Type to search..."
         />
-        <button type="button">
-          <svg
-            className="fill-[#666] w-6 h-6 m-0"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-          >
-            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-          </svg>
-        </button>
+
+        <div className="flex items-center">
+          <p>Price</p>
+          <div className="text-xs ml-2">
+            <button type="button" onClick={onSortAsc}>
+              Low-High
+            </button>
+            <button type="button" onClick={onSortDesc}>
+              High-Low
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,48 @@ import Searchbar from "../components/Searchbar";
 import PlantCard from "../components/PlantCard";
 import plants from "../data/plants";
 import bgImage from "../assets/img/banner-all-plnts.avif";
+import { useState } from "react";
 
 const Home = () => {
+  const [sortOrder, setSortOrder] = useState(null);
+
+  const handleSortAsc = () => {
+    if (sortOrder === "asc") {
+      setSortOrder(null);
+    } else {
+      setSortOrder("asc");
+    }
+  };
+
+  const handleSortDesc = () => {
+    if (sortOrder === "desc") {
+      setSortOrder(null);
+    } else {
+      setSortOrder("desc");
+    }
+  };
+
+  const sortedData = () => {
+    if (sortOrder === "asc") {
+      return [...plants].sort((a, b) => a.price - b.price);
+    } else if (sortOrder === "desc") {
+      return [...plants].sort((a, b) => b.price - a.price);
+    } else {
+      return plants;
+    }
+  };
+
   return (
     <main className="flex-grow overflow-y-auto">
-      <section
-        className="bg-cover bg-no-repeat  bg-center relative"
-        style={{
-          backgroundImage: `url(${bgImage})`,
-        }}
-      >
+      <section className="bg-cover bg-no-repeat bg-center relative">
         <div>
-          <Searchbar />
+          <Searchbar onSortAsc={handleSortAsc} onSortDesc={handleSortDesc} />
         </div>
       </section>
       <section>
         <div className="flex items-center justify-center py-20">
           <div className="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-2 max-w-6xl">
-            {plants.map((plant) => (
+            {sortedData().map((plant) => (
               <PlantCard
                 key={plant.id}
                 name={plant.name}
