d3ff9d2c886a50998cd70068b2c733f866e6e520
tina otarashvili <tinatinotarashvili99@gmail.com>
2024-04-03 01:00:19 +0400

    Initialize next.js app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  // other Tailwind CSS settings...
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
 };
