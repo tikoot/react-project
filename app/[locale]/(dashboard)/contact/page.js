849240c84020f6399e7258bceb145aff44fc1bfd
tina otarashvili <tinatinotarashvili99@gmail.com>
2024-04-25 10:51:44 +0400

    update code

--- app/[locale]/(dashboard)/contact/page.js
+++ app/[locale]/(dashboard)/contact/page.js
@@ -5,56 +5,100 @@ import { useTranslations } from "next-intl";
 export default function Contact() {
   const t = useTranslations("Contact");
   return (
-    <div className="bg-gray-200 dark:bg-slate-500 flex flex-1 p-8 justify-center">
-      <form action="/submit-form">
-        <div className=" bg-gray-100 p-8  w-[550px]">
-          <h1 className="text-xl font-medium">{t("message")}</h1>
-          <ContactForm
-            type="text"
-            label={t("name-label")}
-            placeholder={t("name")}
-            id="fullName"
-          />
-          <ContactForm
-            type="text"
-            label={t("email-label")}
-            placeholder={t("email")}
-            id="Email"
-          />
-          <ContactForm
-            type="text"
-            label={t("subject")}
-            placeholder={t("subject")}
-            id="subject"
-          />
-          <ContactForm type="textarea" label={t("textarea")} id="textarea" />
-
-          <button
-            className="bg-gray-800 hover:bg-gray-900 transition text-white px-4 py-2 rounded w-36 mt-4 ml-4"
-            type="submit"
-          >
-            {t("send-button")}
-          </button>
+    <div className="flex-grow overflow-y-auto bg-white">
+      <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
+        <div className="relative px-6 pb-10 pt-24 sm:pt-24 lg:static lg:px-8 lg:py-24">
+          <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
+            <h1>CONTACT US</h1>
+            <h2 className="text-3xl font-bold tracking-tight text-[#4b3049]">
+              LET’S GET IN TOUCH
+            </h2>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Proin volutpat consequat porttitor cras nullam gravida at. Orci
+              molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
+              Arcu sed malesuada et magna.
+            </p>
+            <dl className="mt-10 space-y-4 text-base leading-7 text-gray-600">
+              <div className="flex gap-x-4">
+                <dt className="flex-none">
+                  <span className="sr-only">Address</span>
+                  <svg
+                    className="h-7 w-6 text-[#4b3049] fill-[#4b3049]"
+                    fill="#4b3049"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
+                    />
+                  </svg>
+                </dt>
+                <dd>
+                  545 Mavis Island
+                  <br />
+                  Chicago, IL 99191
+                </dd>
+              </div>
+              <div className="flex gap-x-4">
+                <dt className="flex-none">
+                  <span className="sr-only">Telephone</span>
+                  <svg
+                    className="h-7 w-6 text-gray-400"
+                    fill="#4b3049"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+                    />
+                  </svg>
+                </dt>
+                <dd>
+                  <a
+                    className="hover:text-gray-900"
+                    href="tel:+1 (555) 234-5678"
+                  >
+                    +1 (555) 234-5678
+                  </a>
+                </dd>
+              </div>
+              <div className="flex gap-x-4">
+                <dt className="flex-none">
+                  <span className="sr-only">Email</span>
+                  <svg
+                    className="h-7 w-6 text-gray-400"
+                    fill="#4b3049"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                    />
+                  </svg>
+                </dt>
+                <dd>
+                  <a
+                    className="hover:text-gray-900"
+                    href="mailto:hello@example.com"
+                  >
+                    hello@lorem.com
+                  </a>
+                </dd>
+              </div>
+            </dl>
+          </div>
         </div>
-      </form>
-      <div className="bg-cyan-600 p-8">
-        <h1 className="text-xl font-medium">{t("contact-us")}</h1>
-        <CompanyContact
-          image="/images/contact/location.png"
-          companyInfo="Suite 293 95409 Grimes Crossing"
-        />
-        <CompanyContact
-          image="/images/contact/phone.png"
-          companyInfo="638 891 206 "
-        />
-        <CompanyContact
-          image="/images/contact/website.png"
-          companyInfo="www.web-site. com"
-        />
-        <CompanyContact
-          image="/images/contact/mail.png"
-          companyInfo="pas-uwufuba91@mail.com"
-        />
+        <ContactForm />
       </div>
     </div>
   );
