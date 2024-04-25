c5a89cbbd0b593181437ddeea4554be1115184f0
tina otarashvili <tinatinotarashvili99@gmail.com>
2024-04-25 14:23:53 +0400

    fix cpde

--- app/[locale]/(dashboard)/blog/page.js
+++ app/[locale]/(dashboard)/blog/page.js
@@ -1,18 +1,69 @@
-import BlogPage from "@/components/BlogPage";
+import PlantCard from "@/components/PlantCard";
 
-async function getBlogsData() {
-  const res = await fetch("https://dummyjson.com/recipes");
+async function getProductsData() {
+  const res = await fetch("https://dummyjson.com/products");
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
+
   return res.json();
 }
 
-export default async function Blog() {
-  const blogs = await getBlogsData();
+export default async function Blogs() {
+  const itemsData = await getProductsData();
 
-  return <BlogPage data={blogs} />;
+  return (
+    <>
+      <section className="bg-white py-20 overflow-y-auto">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl text-center">
+            <p className="mt-2 text-4xl font-bold text-[#B08EAD] capitalize">
+              Blogs
+            </p>
+          </div>
+
+          <div className="text-center mt-10">
+            <p className="text-gray-500">Loading...</p>
+          </div>
+
+          <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <div className="flex items-center justify-center py-20">
+              <div className="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-2 max-w-6xl">
+                {itemsData &&
+                  itemsData.map((product) => (
+                    <a key={product.id} href={`/product/${product.id}`}>
+                      <PlantCard
+                        id={product.id}
+                        name={product.title}
+                        description={product.description}
+                        image={product.thumbnail}
+                        price={product.price}
+                      />
+                    </a>
+                  ))}
+              </div>
+            </div>
+            {/* {blogs && blogs.length > 0 ? (
+            blogs.map((blog, index) => (
+              <a key={index} href={`/blogs/${blog.id}`}>
+                <BlogPost
+                  key={index}
+                  title={blog.title}
+                  description={blog.body}
+                  image={blog.image}
+                  date={blog.release_date}
+                />
+              </a>
+            ))
+          ) : (
+            <p>No blog posts found.</p>
+          )} */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export { getBlogsData };
+export { getProductsData };
--- app/[locale]/(dashboard)/profile/page.js
+++ app/[locale]/(dashboard)/profile/page.js
@@ -1,39 +1,114 @@
-import ProfileForm from "@/components/ProfileForm";
-import { useTranslations } from "next-intl";
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
 
-export default function Profile() {
-  const t = useTranslations("Profile");
-  const inputData = [
-    { type: "text", id: "username", placeholder: t("username") },
-    { type: "text", id: "surname", placeholder: t("surname") },
-    { type: "text", id: "email", placeholder: t("email") },
-    { type: "password", id: "password", placeholder: t("password") },
-    {
-      type: "password",
-      id: "confirmPassword",
-      placeholder: t("confirmPass"),
-    },
-  ];
+const ProfileInfo = () => {
+  const [formData, setFormData] = useState({
+    name: "tiko",
+    lastname: "otarashvili",
+    email: "tinatinotarashvili99@gmail.com",
+    password: "",
+    confirmPassword: "",
+  });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
-    <section className="flex flex-1 bg-gray-200 dark:bg-slate-500 justify-center p-4">
-      <form className="flex flex-col bg-[#B85042]  dark:bg-slate-600 w-96 p-4 gap-2">
-        {inputData.map((input) => (
-          <ProfileForm
-            key={input.id}
-            type={input.type}
-            id={input.id}
-            placeholder={input.placeholder}
-          />
-        ))}
+    <>
+      <section className="flex flex-col h-screen overflow-y-auto">
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <Image
+              src="/img/avatar.png"
+              alt="Plant Logo"
+              width={120}
+              height={120}
+              className="  rounded-full"
+            />
+          </div>
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" onSubmit={handleSubmit}>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  readOnly
+                  className="disabled bg-transparent focus:outline-none border-b border-transparent capitalize"
+                  style={{ caretColor: "transparent" }}
+                />
+              </div>
 
-        <button
-          className=" hover:bg-slate-300 bg-slate-200   dark:bg-slate-500 transition px-4 py-2  mx-auto mt-8"
-          type="submit"
-        >
-          {t("button")}
-        </button>
-      </form>
-    </section>
+              <div className="mt-2">
+                <input
+                  id="lastname"
+                  name="lastname"
+                  value={formData.lastname}
+                  readOnly
+                  className="disabled bg-transparent focus:outline-none border-b border-transparent capitalize"
+                  style={{ caretColor: "transparent" }}
+                />
+              </div>
+
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={formData.email}
+                  readOnly
+                  className="disabled bg-transparent focus:outline-none border-b border-transparent w-full"
+                  style={{ caretColor: "transparent" }}
+                />
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="New Password"
+                  required
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 pl-2 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <div className="mt-2">
+                <input
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="Confirm Password"
+                  required
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 pl-2 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-[#B08EAD] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm"
+                >
+                  Save
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
   );
-}
+};
+
+export default ProfileInfo;
