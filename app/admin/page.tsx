5393355ef5f140420f23c3af9ccd72d7978ca1df
tina otarashvili <tinatinotarashvili99@gmail.com>
2024-05-19 20:29:29 +0400

    remove files to refactor code

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,87 +1,91 @@
-import { createUserAction } from "../../actions";
-import { User, getUsers } from "../../api";
 export default async function Admin() {
-  const users = await getUsers();
-  return (
-    <div className="px-4 sm:px-6 lg:px-8 py-8">
-      <form action={createUserAction}>
-        <input type="text" name="name" />
-        <input type="text" name="email" />
-        <button type="submit">Create User</button>
-      </form>
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">
-            Users
-          </h1>
-        </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <button
-            type="button"
-            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm "
-          >
-            Add user
-          </button>
-        </div>
-      </div>
-      <div className="mt-8 flow-root">
-        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <table className="min-w-full divide-y divide-gray-300">
-              <thead>
-                <tr>
-                  <th
-                    scope="col"
-                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
-                  >
-                    Name
-                  </th>
-
-                  <th
-                    scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                  >
-                    Email
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                  >
-                    age
-                  </th>
-                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                    <span className="sr-only">Delete</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-200">
-                {users.map((user: User) => (
-                  <tr key={user.id}>
-                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                      {user.name}
-                    </td>
-
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {user.email}
-                    </td>
-                    {/* <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {user.age}
-                    </td> */}
-                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                      <a
-                        href="#"
-                        className="text-indigo-600 hover:text-indigo-900"
-                      >
-                        Delete
-                      </a>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  <div>admin</div>;
 }
+
+// import { createUserAction } from "../../actions";
+// import { User, getUsers } from "../../api";
+// export default async function Admin() {
+//   const users = await getUsers();
+//   return (
+//     <div className="px-4 sm:px-6 lg:px-8 py-8">
+//       <form action={createUserAction}>
+//         <input type="text" name="name" />
+//         <input type="text" name="email" />
+//         <button type="submit">Create User</button>
+//       </form>
+//       <div className="sm:flex sm:items-center">
+//         <div className="sm:flex-auto">
+//           <h1 className="text-base font-semibold leading-6 text-gray-900">
+//             Users
+//           </h1>
+//         </div>
+//         <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+//           <button
+//             type="button"
+//             className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm "
+//           >
+//             Add user
+//           </button>
+//         </div>
+//       </div>
+//       <div className="mt-8 flow-root">
+//         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+//           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+//             <table className="min-w-full divide-y divide-gray-300">
+//               <thead>
+//                 <tr>
+//                   <th
+//                     scope="col"
+//                     className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+//                   >
+//                     Name
+//                   </th>
+
+//                   <th
+//                     scope="col"
+//                     className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+//                   >
+//                     Email
+//                   </th>
+//                   <th
+//                     scope="col"
+//                     className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+//                   >
+//                     age
+//                   </th>
+//                   <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+//                     <span className="sr-only">Delete</span>
+//                   </th>
+//                 </tr>
+//               </thead>
+//               <tbody className="divide-y divide-gray-200">
+//                 {users.map((user: User) => (
+//                   <tr key={user.id}>
+//                     <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+//                       {user.name}
+//                     </td>
+
+//                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+//                       {user.email}
+//                     </td>
+//                     {/* <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+//                       {user.age}
+//                     </td> */}
+//                     <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+//                       <a
+//                         href="#"
+//                         className="text-indigo-600 hover:text-indigo-900"
+//                       >
+//                         Delete
+//                       </a>
+//                     </td>
+//                   </tr>
+//                 ))}
+//               </tbody>
+//             </table>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
