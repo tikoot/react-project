0f3833614371c322db02eff10e865b44d878646c
tina otarashvili <tinatinotarashvili99@gmail.com>
2024-05-19 20:06:11 +0400

    fix

--- actions.ts
+++
@@ -1,7 +0,0 @@
-import { createUser } from "./api";
-export async function createUserAction(formData: FormData) {
-  "use server";
-  const { name, email, age } = Object.fromEntries(formData);
-
-  return createUser(name as string, email as string, age as string);
-}
