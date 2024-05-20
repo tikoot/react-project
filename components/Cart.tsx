986a653eaac7d6346825f5e63f289614b26116b5
tikoot <tiko.otarashvili99@gmail.com>
2024-05-20 16:44:07 +0400

    fix-styles

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 const Cart = ({ selectedNumber, isClient }: any) => {
   return (
     <Link href={"/checkout"}>
-      <div className="cart-container">
+      <div className="">
         <img src="/img/grocery-store.png" alt="" className="w-4 h-4" />
-        <span className="quantity">{isClient ? selectedNumber : 0}</span>
+        <span className="">{isClient ? selectedNumber : 0}</span>
       </div>
     </Link>
   );
